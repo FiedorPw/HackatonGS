6c18e44a98f2d4a7b055ffbf161109efff24a756
YouRekt <czarnecki.jurko@gmail.com>
2024-01-20 00:42:50 +0100

    PoC

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import psycopg2
 from flask import Flask, render_template, request, redirect, url_for, flash
+import requests
 
 app = Flask(__name__)
 os.environ['DB_USERNAME'] = 'admin'
@@ -8,9 +9,11 @@ os.environ['DB_PASSWORD'] = 'admin'
 
 message_list = []
 
-#SuperZbazowanaBaza#GS
 
-#łaczymy sie username,password zapisanymi w global variables
+
+# SuperZbazowanaBaza#GS
+
+# łaczymy sie username,password zapisanymi w global variables
 def get_db_connection():
     conn = psycopg2.connect(host='localhost',
                             database='flask_db',
@@ -18,12 +21,12 @@ def get_db_connection():
                             password=os.environ['DB_PASSWORD'])
     return conn
 
-def get_from_database_by_name(db_connection,cur,text):
+
+def get_from_database_by_name(db_connection, cur, text):
     try:
         query = f"SELECT * FROM students WHERE imie='{text}';"
         cur.execute(query)
         wyszukana_osoba = cur.fetchone()
-
 
         if wyszukana_osoba is None:
             wyszukana_osoba = ''
@@ -37,26 +40,28 @@ def get_from_database_by_name(db_connection,cur,text):
         cur.close()
         return wyszukana_osoba
     pass
-@app.route('/', methods=['POST','GET'])
-def index():
 
-    #inicjalizacja kursora do pobierania danych z db
+
+@app.route('/', methods=['POST', 'GET'])
+def index():
+    # inicjalizacja kursora do pobierania danych z db
     db_connection = get_db_connection()
     cur = db_connection.cursor()
     students = ""
     wyszukana_osoba = ""
 
-    #wysyłamy strone przy ładowaniu
+    # wysyłamy strone przy ładowaniu
     if request.method == "GET":
         cur.execute('SELECT * FROM students OFFSET 1;')
         students = cur.fetchall()
         cur.close()
         db_connection.close()
-    #odpowiedz na klika "Get student's data"
+    # odpowiedz na klika "Get student's data"
     if request.method == "POST":
         text = request.form['text']
-        wyszukana_osoba = get_from_database_by_name(db_connection,cur,text)
+        wyszukana_osoba = get_from_database_by_name(db_connection, cur, text)
     return render_template('index2.html', students=students, wyszukana_osoba=wyszukana_osoba)
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -70,19 +75,18 @@ def login():
         query = f"SELECT haslo FROM students WHERE imie='{username}';"
         cur.execute(query)
         password_from_db = cur.fetchone()
-        #sprawdzamy czy dla podanego użytkownika hasło w bazie zgadza sie z podanym
+        # sprawdzamy czy dla podanego użytkownika hasło w bazie zgadza sie z podanym
         if password == str(password_from_db[0]):
             loggedIN = "you are logged in!"
             return redirect(url_for('index'))
         else:
             loggedIN = "wrong credentials"
-    return render_template('login.html',loggedIN=loggedIN)
+    return render_template('login.html', loggedIN=loggedIN)
 
 
 @app.route('/chat', methods=['POST', 'GET'])
 def chat():
     # inicjalizacja kursora do pobierania danych z db
-
 
     # wysyłamy strone przy ładowaniu
     if request.method == "GET":
@@ -90,8 +94,16 @@ def chat():
     # odpowiedz na klika "Get student's data"
 
     if request.method == "POST":
-        text = request.form['text']
-        print(text)
+        message = request.form['text']
+        print(message)
+
+        userMessage = {"user_id": 1, "message": str(message)}
+
+        response = requests.post('http://127.0.0.1:8000/assistant/', json=userMessage)
+        print(response.json())
+        return response.json()
+
     return render_template('index2.html')
+
 
 
