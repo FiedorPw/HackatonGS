3fc25cd2628e497fb580cf87e3ed968443fdaf6a
mvishiu11 <116217532+mvishiu11@users.noreply.github.com>
2024-01-20 09:15:02 +0100

    feat: make files unique

--- app.py
+++ app.py
@@ -45,6 +45,8 @@ def upload_file():
     file = request.form['message']
     logger.info(f'Received user file: {file}')
     files.append(file)
+    new_files = []
+    [new_files.append(file) for file in files if file not in new_files]
     return jsonify({'file': file})
 
 @app.route('/login')
