12b54b4fb819874bb05533a48d0f738b4dce4c06
mvishiu11 <116217532+mvishiu11@users.noreply.github.com>
2024-01-20 04:17:07 +0100

    fix: add app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, jsonify, request, render_template
+import requests
 
 app = Flask(__name__)
 messages = []
@@ -17,6 +18,10 @@ def get_messages():
 def send_message():
     message = request.form['message']
     messages.append(message)
+    print("request: " + str(request))
+    response = requests.post('http://localhost:8000/assistant/', json={'user_id' : 1, 'message': message})
+    print(response.json())
+    messages.append(response.json()['message'])
     return jsonify(success=True)
 
 if __name__ == '__main__':
